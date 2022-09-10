61c3c227a9c881537f91d0fef3e98f4fd4d865b8
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-09-10 10:09:48 +0700

    Get data from input

--- src/Components/ValidationForm/DemoValidationForm/ValidationForm.js
+++ src/Components/ValidationForm/DemoValidationForm/ValidationForm.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import "./ValidationForm.css";
 export default class ValidationForm extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  };
+
+
+  handleChangeValue = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+        [name]: value,
+    },()=>{
+        console.log(this.state);
+    });
+  }
   render() {
     return (
       <div className=" d-flex justify-content-center">
         <form className="w-50 bg-info bg-gradient p-5 m-5">
-          <h1 className="text-primary text-center">User Profile</h1>
+          <h1 className="text-primary text-center p-3">User Profile</h1>
           <div className="container">
             <div className="row">
               <div className="col-6">
                 <div className="group">
-                  <input type="text" name="firstName" required />
+                  <input type="text" name="firstName" required onChange={(event)=>{this.handleChangeValue(event)}} />
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>First Name</label>
@@ -19,7 +37,7 @@ export default class ValidationForm extends Component {
 
               <div className="col-6">
                 <div className="group">
-                  <input type="text" name="lastName" required />
+                  <input type="text" name="lastName" required onChange={(event)=>{this.handleChangeValue(event)}} />
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>Last Name</label>
@@ -30,7 +48,7 @@ export default class ValidationForm extends Component {
             <div className="row">
               <div className="col-12">
                 <div className="group">
-                  <input type="text" name="userName" required />
+                  <input type="text" name="userName" required onChange={(event)=>{this.handleChangeValue(event)}}/>
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>User Name</label>
@@ -41,7 +59,7 @@ export default class ValidationForm extends Component {
             <div className="row">
               <div className="col-12">
                 <div className="group">
-                  <input type="email" name="email" required />
+                  <input type="email" name="email" required onChange={(event)=>{this.handleChangeValue(event)}} />
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>Email</label>
@@ -49,10 +67,10 @@ export default class ValidationForm extends Component {
               </div>
             </div>
 
-            <div className="row">   
+            <div className="row">
               <div className="col-6">
                 <div className="group">
-                  <input type="password" name="password" required />
+                  <input type="password" name="password" required onChange={(event)=>{this.handleChangeValue(event)}} />
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>Password</label>
@@ -61,7 +79,7 @@ export default class ValidationForm extends Component {
 
               <div className="col-6">
                 <div className="group">
-                  <input type="password" name="passwordConfirm" required />
+                  <input type="password" name="passwordConfirm" required onChange={(event)=>{this.handleChangeValue(event)}} />
                   <span className="highlight"></span>
                   <span className="bar"></span>
                   <label>Password Confirm</label>
@@ -70,7 +88,9 @@ export default class ValidationForm extends Component {
             </div>
 
             <div className="text-center">
-              <button className="btn btn-success w-100 p-3 col-12">Submit</button>
+              <button className="btn btn-success w-100 p-3 col-12">
+                Submit
+              </button>
             </div>
           </div>
         </form>
